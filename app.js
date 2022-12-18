d832f7170ff7ffaaac2b83ff0f3dfb963347c27b
mahmoud khalid <mahmoud0122549@gmail.com>
2022-12-18 23:23:02 +0200

    preloader

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const icon = document.querySelector(".menu-icon");
 const list = document.querySelector(".navigation ul ");
 const close = document.querySelector(".fa-x");
+const preloader = document.querySelector(".preloader");
 
 icon.addEventListener("click", () => {
   list.style.display = "block";
@@ -11,4 +12,8 @@ close.addEventListener("click", () => {
   list.style.display = "none";
   close.style.display = "none";
   icon.style.display = "block";
+});
+
+window.addEventListener("load", () => {
+  preloader.classList.add("hide-preloader");
 });
