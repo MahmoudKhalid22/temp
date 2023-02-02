d79b62103f70784860f8dd987609745042479803
mahmoud khalid <mahmoud0122549@gmail.com>
2023-02-02 16:26:20 +0200

    adding some animations

--- app.js
+++ app.js
@@ -2,6 +2,13 @@ const menuIcon = document.querySelector(".menu-icon");
 list = document.querySelector(".nav-links");
 const preloader = document.querySelector(".preloader");
 const scroller = document.querySelector(".scroller");
+const features = document.querySelectorAll(".feature_one");
+const featureLeft = document.querySelector(".feature_left")
+const featureRight = document.querySelector(".feature_right")
+const serviceBoxes = document.querySelectorAll(".s-h");
+// const 
+// const 
+
 
 // preloader
 window.addEventListener("load", () => {
@@ -24,3 +31,37 @@ function updateProgressBar() {
 window.addEventListener("scroll", updateProgressBar);
 
 // animation
+window.addEventListener("scroll", animationBoxes);
+
+function animationBoxes() {
+  const triggerBottom = (window.innerHeight / 5) * 4;
+
+  features.forEach((box) => {
+    const boxTop = box.getBoundingClientRect().top;
+    if (boxTop > triggerBottom) {
+      box.classList.add("feature_remove_blur");
+    }
+  });
+  const leftBottom = featureLeft.getBoundingClientRect().top;
+  const rightBottom = featureRight.getBoundingClientRect().top;
+
+  if (leftBottom > triggerBottom){
+    featureLeft.classList.toggle("feature_remove_left")
+  }
+  if (rightBottom > triggerBottom){
+    featureRight.classList.toggle("feature_remove_right")
+  }
+}
+
+window.addEventListener("scroll", animationServiceBoxes);
+function animationServiceBoxes() {
+  const serviceBottomTrigger = (window.innerHeight / 5) * 4;
+  serviceBoxes.forEach(box =>{
+    const boxTop = box.getBoundingClientRect().top;
+    if(serviceBottomTrigger < boxTop){
+        box.classList.add("s-h_remove_blur");
+        box.classList.add("s-h_remove_left")
+    }
+  })
+}
+
