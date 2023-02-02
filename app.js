35e4b0627eb77d4c3ff440fafe93815f9a49788b
mahmoud khalid <mahmoud0122549@gmail.com>
2023-02-02 10:50:12 +0200

    progress bar

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
 const menuIcon = document.querySelector(".menu-icon");
 list = document.querySelector(".nav-links");
 const preloader = document.querySelector(".preloader");
-
+const scroller = document.querySelector(".scroller");
+// preloader
 window.addEventListener("load", () => {
   preloader.classList.add("hide-preloader");
 });
-
+// navbar
 menuIcon.addEventListener("click", () => {
   list.classList.toggle("showList");
 });
+
+// scroll
+function updateProgressBar() {
+  const { scrollTop, scrollHeight } = document.documentElement;
+  const scrollPercentage = `${
+    (scrollTop / (scrollHeight - window.innerHeight)) * 100
+  }%`;
+  scroller.style.width = scrollPercentage;
+}
+
+window.addEventListener("scroll", updateProgressBar);
